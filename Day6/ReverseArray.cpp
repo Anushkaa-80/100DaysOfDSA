13bc9604d3b15b1efd87a70a0a567fdf3e0d71d1
Anushkaa-80 <anushkaa8090@gmail.com>
2025-09-09 16:00:41 +0530

    Add palindrome string functionality and include string.h header

--- Day6/ReverseArray.cpp
+++ Day6/ReverseArray.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <bits/stdc++.h>
+#include<string.h>
 using namespace std;
 
 void reverseArray(int i, int arr[], int n)
@@ -18,6 +19,13 @@ void reverse2(int l,int arr[], int r)
     reverse2(l+1,arr,r-1);
 }
 
+void palindromeString(int l, char s[], int r)
+{
+    if(l>=r)
+       return;
+    swap(s[l],s[r]);
+    palindromeString(l+1,s,r-1);
+}
 
 int main()
 {
@@ -36,6 +44,17 @@ int main()
     reverse2(0,arr,n-1);
     for(int j=0;j<n;j++)
      cout<<arr[j]<<" ";
-
+     char s[20],temp[20];
+     cin>>s;
+     strcpy(temp,s);
+     palindromeString(0,s,strlen(s)-1);
+     
+     if(strcmp(temp,s)==0)
+     {
+        cout<<"Palindrome";
+     }
+     else{
+        cout<<"Not palindrome";
+     }
 
 }
