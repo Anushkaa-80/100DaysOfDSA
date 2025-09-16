633cabec28c78d28bb89dca99055c06d79ac2cfa
Anushkaa-80 <anushkaa8090@gmail.com>
2025-09-16 23:43:04 +0530

    Add insertion sort implementation

--- Day-8/Sorting.cpp
+++ Day-8/Sorting.cpp
@@ -41,6 +41,21 @@ void bubble_sort(int arr[], int n)
 
 }
 
+void insertion_sort(int arr[], int n)
+{
+    for(int i=0;i<=n-1;i++)
+    {
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j])
+        {
+            int temp= arr[j-1];
+            arr[j-1]= arr[j];
+            arr[j]=temp;
+
+            j--;
+        }
+    }
+}
 
 
 int main()
@@ -53,7 +68,8 @@ int main()
     for(int i=0;i<n;i++)
       cin>>arr[i];
     //selection_sort(arr,n);
-    bubble_sort(arr,n);
+  //  bubble_sort(arr,n);
+  insertion_sort(arr,n);
     cout<<"Sorted Array: ";
      for(int i=0;i<n;i++)
       cout<<arr[i]<<" ";
