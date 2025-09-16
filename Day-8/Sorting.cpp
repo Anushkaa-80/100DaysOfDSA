19a006fcf46587635a2cfcb6c9cc6c5126fa493c
Anushkaa-80 <anushkaa8090@gmail.com>
2025-09-16 23:25:25 +0530

    Optimize bubble sort by adding early exit condition

--- Day-8/Sorting.cpp
+++ Day-8/Sorting.cpp
@@ -23,6 +23,7 @@ void bubble_sort(int arr[], int n)
 {
     for(int i=n-1;i>=0;i--)
     {
+        int didSwap=0; //optimization
         for(int j=0;j<=i-1;j++)
         {
             if(arr[j]>arr[j+1])
@@ -30,8 +31,11 @@ void bubble_sort(int arr[], int n)
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]= temp;
+                didSwap=1;
             }
         }
+        if(didSwap==0)
+            break;
     }
 
 
