b57a4fc5d24f452f54f17a0654798d4e40f6b723
Anushkaa-80 <anushkaa8090@gmail.com>
2025-09-16 23:20:26 +0530

    add bubble sort implementation

--- Day-8/Sorting.cpp
+++ Day-8/Sorting.cpp
@@ -19,6 +19,25 @@ void selection_sort(int arr[],int n)
     }
 }
 
+void bubble_sort(int arr[], int n)
+{
+    for(int i=n-1;i>=0;i--)
+    {
+        for(int j=0;j<=i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]= temp;
+            }
+        }
+    }
+
+
+}
+
+
 
 int main()
 {
@@ -29,7 +48,8 @@ int main()
     int arr[n];
     for(int i=0;i<n;i++)
       cin>>arr[i];
-    selection_sort(arr,n);
+    //selection_sort(arr,n);
+    bubble_sort(arr,n);
     cout<<"Sorted Array: ";
      for(int i=0;i<n;i++)
       cout<<arr[i]<<" ";
