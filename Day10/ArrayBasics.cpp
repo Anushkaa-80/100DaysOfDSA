96b190473f551e23d8842b69a28be92340b80a00
Anushkaa-80 <anushkaa8090@gmail.com>
2025-10-02 19:33:06 +0530

    implemented  remove duplicates

--- Day10/ArrayBasics.cpp
+++ Day10/ArrayBasics.cpp
@@ -55,15 +55,52 @@ int second_smallest(int arr[], int n)
     return small2;
 
 }
+bool is_sorted(int arr[], int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]>arr[i-1])
+        {
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int remove_duplicates(int arr[], int n)
+{
+    int i=0;
+    for(int j=1;j<n;j++)
+    {
+        if(arr[j]!=arr[i])
+        {
+            i++;
+            arr[i]=arr[j];
+            
+        }
+
+    }
+    return (i+1);
+}
 
 
 int main()
 {
-    int arr[7]={3,2,1,5,6,8,4};
-    int l=largest(arr);
-    cout<<"Largest "<<l;
-    int sl=secondLargest(arr,7);
-    cout<<"\nsecond largest"<<sl;
-     cout<<"Second smallest"<<second_smallest(arr,7);
+    int arr[7]={1,1,2,2,3,3,3};
+    // int l=largest(arr);
+    // cout<<"Largest "<<l;
+    // int sl=secondLargest(arr,7);
+    // cout<<"\nsecond largest"<<sl;
+    //  cout<<"Second smallest"<<second_smallest(arr,7);
+    //  cout<<"\nSorted"<<is_sorted(arr,7);
+
+     int index=remove_duplicates(arr,7);
+     for(int i=0;i<index;i++)
+     { 
+     cout<<arr[i];
+     }
 return 0;
 }
---
+++ Day10/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+int l=largest(arr);
+    // cout<<"Largest "<<l;
+    // int sl=secondLargest(arr,7);
+    // cout<<"\nsecond largest"<<sl;
+    //  cout<<"Second smallest"<<second_smallest(arr,7);
+    //  cout<<"\nSorted"<<is_sorted(arr,7);
