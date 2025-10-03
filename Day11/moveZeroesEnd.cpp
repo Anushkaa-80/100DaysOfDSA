93f488a962ab6dcea934bb7817fa0d1dd9eb4afb
Anushkaa-80 <anushkaa8090@gmail.com>
2025-10-04 01:15:44 +0530

    optimal solution for zeroes at the end

--- Day11/moveZeroesEnd.cpp
+++ Day11/moveZeroesEnd.cpp
@@ -27,6 +27,34 @@ vector<int> brute(vector<int> arr, int n)
     }
     return arr;
 }
+void optimal(int arr[], int n)
+{
+    //s1: we neeed two pointers i and j
+    // i points to non-zero and j points to zero
+    // s2: check i is non-zero, and j==0
+    /// swapthe i and j
+
+    //step1: pointing j to zero
+    int j=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            j=i;
+            break;
+        }
+    }
+
+    //step2:
+    for(int i=j+1;i<n;i++)
+    {
+        if(arr[i]!=0)
+        {
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+}
 int main()
 {
     vector<int > arr={1,0,2,3,2,0,0,4,5,1};
@@ -39,6 +67,14 @@ int main()
     for(int i=0;i<n;i++)
     {
         cout<<arr[i];
+    }
+    int arr2[10]={1,0,2,3,2,0,0,4,5,1};
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    optimal(arr2,n);
+    cout<<"\n Optimal solution is:";
+     for(int i=0;i<n2;i++)
+    {
+        cout<<arr2[i];
     }
 
 }
