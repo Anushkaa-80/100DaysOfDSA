67dd54803836832c0ae07c46eabaee5ad8bc7b47
Anushkaa-80 <anushkaa8090@gmail.com>
2025-10-05 16:58:42 +0530

    implemented intersection optimal approach

--- Day11/UnionIntersection.cpp
+++ Day11/UnionIntersection.cpp
@@ -30,14 +30,86 @@ vector<int> bruteUnion(vector<int> a, vector<int> b)
     return temp;
 
 }
+vector<int> optimal(vector<int> a, vector<int>b)
+{
+    int n1=a.size();
+    int n2=b.size();
+    int i=0, j=0;
+    vector<int> unionArr;
+    while(i<n1 && j<n2)
+    {
+        if(a[i]<=b[j])
+        {
+            if(unionArr.size()==0 || unionArr.back()!=0)
+            {
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        }
+        else{
+            if(unionArr.size()==0 || unionArr.back()!=b[j])
+            {
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
 
+    }
+    while(j<n2)
+    {
+        if(unionArr.size()==0 || unionArr.back()!=b[j])
+        {
+            unionArr.push_back(b[j]);
+        }
+        j++;
+
+    }
+      while(i<n1)
+    {
+        if(unionArr.size()==0 || unionArr.back()!=a[i])
+        {
+            unionArr.push_back(a[i]);
+        }
+    i++;
+
+    }
+return unionArr;
+}
+vector<int> intersection(vector<int> A, vector<int> B)
+{
+    int i=0, j=0;
+    int n= A.size();
+    int m=B.size();
+
+    vector<int> ans;
+
+   while(i<n && j<m)
+   {
+    if(A[i]<B[j])
+    {
+        i++;
+    }
+    else if(B[j]<A[i])
+    {
+        j++;
+    }
+    else{
+        ans.push_back(A[i]);
+        i++;
+        j++;
+    }
+
+   }
+    return ans;
+
+}
 int main()
 {
-   vector<int> a={1,1,2,3,4,5,6};
+   vector<int> a={1,1,2,3,4,4,5,6};
    vector<int> b={2,3,4,4,5};
 
-   vector<int> arr=bruteUnion(a,b);
-   
+   vector<int> arr=intersection(a,b);
+
    for(int it=0;it<arr.size();it++)
    {
     cout<<arr[it];
